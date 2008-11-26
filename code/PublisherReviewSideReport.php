11d7a4ead17813c1533616aab3deca24d63b91d2
Ingo Schommer <ingo@silverstripe.com>
2008-11-26 00:31:14 +0000

    MINOR Documentation

--- code/PublisherReviewSideReport.php
+++ code/PublisherReviewSideReport.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * Adds a new "sidereport" in the CMS listing all pages which require publication.
+ * 
+ * @package cmsworkflow
+ */
 class PublisherReviewSideReport extends SideReport {
 	function title() {
 		return _t('PublisherReviewSideReport.TITLE',"Needing to be published");
