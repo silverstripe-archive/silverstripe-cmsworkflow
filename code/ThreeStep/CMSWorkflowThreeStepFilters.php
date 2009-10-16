99beac59a42096bf32eff3ac93e5b913709e3db4
Tom Rix <tom@rixth.org>
2009-10-16 02:14:35 +0000

    BUGFIX: Fixed CMS filters for workflow by making use of new API. (from r88466)

--- code/ThreeStep/CMSWorkflowThreeStepFilters.php
+++ code/ThreeStep/CMSWorkflowThreeStepFilters.php
@@ -10,24 +10,12 @@ class CMSWorkflowThreeStepFilters_PagesAwaitingApproval extends CMSSiteTreeFilte
 	static function title() {
 		return _t('CMSWorkflowThreeStepFilters_PagesAwaitingApproval.TITLE', "Pages awaiting approval");
 	}
-	
-	static function showNode($node) {
-		if ($wf = $node->openWorkflowRequest()) {
-			return $wf->Status == 'AwaitingApproval' ? true : false;
-		}
-		return false;
-	}
-	
-	function getTree() {
-		$leftAndMain = new LeftAndMain();
-		$tree = $leftAndMain->getSiteTreeFor('SiteTree', isset($_REQUEST['ID']) ? $_REQUEST['ID'] : 0, null, array(__CLASS__, 'showNode'));
-	
-		// Trim off the outer tag
-		$tree = ereg_replace('^[ \t\r\n]*<ul[^>]*>','', $tree);
-		$tree = ereg_replace('</ul[^>]*>[ \t\r\n]*$','', $tree);
-	
-		return $tree;
-	}
+
+	function pagesIncluded() {
+		return DB::query('SELECT DISTINCT "SiteTree"."ParentID", "SiteTree"."ID" FROM "SiteTree" 
+			INNER JOIN "WorkflowRequest" ON "WorkflowRequest"."PageID" = "SiteTree"."ID"
+			WHERE "WorkflowRequest"."Status" =  \'AwaitingApproval\'');
+	}	
 }
 
 /**
@@ -41,20 +29,9 @@ class CMSWorkflowThreeStepFilters_PagesAwaitingPublishing extends CMSSiteTreeFil
 		return _t('CMSWorkflowThreeStepFilters_PagesAwaitingPublishing.TITLE', "Pages awaiting publishing");
 	}
 	
-	static function showNode($node) {
-		if ($wf = $node->openWorkflowRequest()) {
-			return $wf->Status == 'Approved' ? true : false;
-		}
-	}
-	
-	function getTree() {
-		$leftAndMain = new LeftAndMain();
-		$tree = $leftAndMain->getSiteTreeFor('SiteTree', isset($_REQUEST['ID']) ? $_REQUEST['ID'] : 0, null, array(__CLASS__, 'showNode'));
-
-		// Trim off the outer tag
-		$tree = ereg_replace('^[ \t\r\n]*<ul[^>]*>','', $tree);
-		$tree = ereg_replace('</ul[^>]*>[ \t\r\n]*$','', $tree);
-
-		return $tree;
-	}
+	function pagesIncluded() {
+		return DB::query('SELECT DISTINCT "SiteTree"."ParentID", "SiteTree"."ID" FROM "SiteTree" 
+			INNER JOIN "WorkflowRequest" ON "WorkflowRequest"."PageID" = "SiteTree"."ID"
+			WHERE "WorkflowRequest"."Status" =  \'Approved\'');
+	}	
 }
