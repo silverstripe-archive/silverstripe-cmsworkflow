f5e0eeeff76707a860e0a40f8760e4e02282d828
Tom Rix <tom@rixth.org>
2009-10-16 02:12:04 +0000

    MINOR sch publishing fix (from r86787)

--- code/ScheduledPublishing.php
+++ code/ScheduledPublishing.php
@@ -14,29 +14,32 @@ class ScheduledPublishing extends BuildTask {
 	}
 	function run($httpRequest) {
 		// Look for changes that need to be published
-		$wfRequests = DataObject::get('WorkflowRequest', "Status = 'Scheduled' AND EmbargoDate <= '".SSDatetime::now()."'");
+		$wfRequests = DataObject::get('WorkflowRequest', "Status = 'Scheduled' AND EmbargoDate <= '".SSDatetime::now()->getValue()."'");
+		$admin = Security::findAnAdministrator();
+		$admin->logIn();
+		
 		if (count($wfRequests)) {
 			foreach($wfRequests as $request) {
-				// Use a try block to prevet one bad requests
+				// Use a try block to prevent one bad request
 				// taking down the whole queue
 				try {
 					if (!$this->suppressOutput) echo "\n<br />Attempting to publish ".$request->Page()->Title.": ";
-					$request->publish('Scheduled publishing', null, false);
+					$request->publish('Page was embargoed. Automatically published.', $admin, false);
 					if (!$this->suppressOutput) echo "ok.";
 				} catch (Exception $e) {
 					// Log it?
 					if (!$this->suppressOutput) echo "fail.";
-					user_error("Error publishing change to Page ID ".$request->PageID." - ".$request->Page()->Title." Error: ".$e->getMessage, E_USER_WARNING);
+					user_error("Error publishing change to Page ID ".$request->PageID." - ".$request->Page()->Title." Error: ".$e->getMessage(), E_USER_WARNING);
 					continue;
 				}
 			}
 		}
 		
 		// Look for live pages that need to be expired
-		$pagesToExpire = Versioned::get_by_stage('SiteTree', 'Live', "ExpiryDate <= '".SSDatetime::now()."'");
+		$pagesToExpire = Versioned::get_by_stage('SiteTree', 'Live', "ExpiryDate <= '".SSDatetime::now()->getValue()."'");
 		if (count($pagesToExpire)) {
 			foreach($pagesToExpire as $page) {
-				// Use a try block to prevet one bad requests
+				// Use a try block to prevent one bad request
 				// taking down the whole queue
 				try {
 					if (!$this->suppressOutput) echo "\n<br />Attempting to unpublish ".$page->Title.": ";
@@ -44,7 +47,7 @@ class ScheduledPublishing extends BuildTask {
 					// Close any existing workflows
 					if ($wf = $page->openWorkflowRequest()) {
 						if (!$this->suppressOutput) echo "closing ".$wf->Status." workflow request: ";
-						$wf->deny('Page automatically expired. Removing from Live site.');
+						$wf->deny('Page automatically expired. Removing from Live site.', $admin);
 						if (!$this->suppressOutput) echo "ok, unpublishing: ";
 					}
 
@@ -56,7 +59,7 @@ class ScheduledPublishing extends BuildTask {
 				} catch (Exception $e) {
 					// Log it?
 					if (!$this->suppressOutput) echo "fail.";
-					user_error("Error unpublishing Page ID ".$page->ID." - ".$page->Title." Error: ".$e->getMessage, E_USER_WARNING);
+					user_error("Error unpublishing Page ID ".$page->ID." - ".$page->Title." Error: ".$e->getMessage(), E_USER_WARNING);
 					continue;
 				}
 			}
