b206cde4c1ebfe524675b210337c9fc7c4c8241c
Luke Hudson <lukeletters@gmail.com>
2009-11-25 20:16:52 +0000

    BUGFIX: Check for admin permissions in 2-step workflow
    This matches the logic in 3-step, where get_by_approver() checks for
    admin/workflow admin permissions.

--- code/WorkflowRequest.php
+++ code/WorkflowRequest.php
@@ -645,9 +645,17 @@ class WorkflowRequest extends DataObject implements i18nEntityProvider {
 		$classesSQL = implode("','", $classes);
 		
 		// build filter
-		$filter = "{$bt}WorkflowRequest_Approvers{$bt}.MemberID = {$approver->ID} 
-			AND {$bt}WorkflowRequest{$bt}.ClassName IN ('$classesSQL')
-		";
+		$filter = '';
+		// check for admin permission
+		if (Permission::checkMember($approver, 'ADMIN') || Permission::checkMember($approver, 'IS_WORKFLOW_ADMIN')) {
+			// Admins can approve/publish anything
+			$filter = "{$bt}WorkflowRequest{$bt}.ClassName IN ('$classesSQL')";
+		} else {
+			$filter = "{$bt}WorkflowRequest_Approvers{$bt}.MemberID = {$approver->ID}
+				AND {$bt}WorkflowRequest{$bt}.ClassName IN ('$classesSQL')
+			";
+		}
+
 		if($status) {
 			$filter .= "AND {$bt}WorkflowRequest{$bt}.Status IN (" . $statusStr . ")";
 		} 
