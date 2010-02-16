6cdf6380ad1ed1c79d7dae56d0ac3588f0683c5b
Tom Rix <tom@rixth.org>
2010-02-16 20:07:39 +0000

    BUGFIX: Use canDeleteFromLive instead of canPublish for checking delete from live action (from r93317) (from r95226)

--- code/WorkflowDeletionRequest.php
+++ code/WorkflowDeletionRequest.php
@@ -59,7 +59,7 @@ class WorkflowDeletionRequest extends WorkflowRequest implements i18nEntityProvi
 
 		// if user doesn't have publish rights, exchange the behavior from
 		// "publish" to "request publish" etc.
-		if(!$page->canPublish() || $openRequest) {
+		if(!$page->canDeleteFromLive() || $openRequest) {
 			// "request removal"
 			$actions->removeByName('action_deletefromlive');
 		}
