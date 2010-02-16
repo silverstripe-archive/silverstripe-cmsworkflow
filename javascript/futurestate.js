76c9f3b4eec61531d8ff783e7a05fb1a6c9a201c
Tom Rix <tom@rixth.org>
2010-02-16 20:58:58 +0000

    FEATURE: Make future state append correct date url (from r97386)

--- javascript/futurestate.js
+++ javascript/futurestate.js
@@ -5,5 +5,29 @@
 			return false;
 		}
 	);
+	
+	$('#FutureStateGoLink').livequery('click',
+		function() {
+			var date = $('#FutureStateDate_Date').val();
+			// Javascript is silly, so we need to convert to US date format first
+			if(date) {
+				var dateParts = date.split('/');
+				date = dateParts[1] + '/' + dateParts[0] + '/' + dateParts[2];
+			} else {
+				date = new Date();
+				date = date.toDateString();
+			}
+			var time = $('#FutureStateDate_Time').val();
+			var dateObj = new Date(date + ' ' + time);
+			
+			var urlDate = dateObj.getFullYear() + '-' + (dateObj.getMonth() + 1) + '-' + dateObj.getDate();
+			urlDate += '+' + dateObj.getHours() + ':' + dateObj.getMinutes() + ':00';
+			
+			var w = window.open(this.href + '?futureDate=' + urlDate, windowName(this.target));
+			w.focus();
+			
+			return false;
+		}
+	);
 
 })(jQuery);
--- templates/FutureStateNavigatorItem.ss
+++ templates/FutureStateNavigatorItem.ss
@@ -1,5 +1,5 @@
 <a id="FutureStateLink"<% if Current %>class="current"<% end_if %>>Future State</a>
 <div id="FutureStateDatePopup">
 	$DateTimeField
-	<a id="FutureStateGoLink">Go</a>
+	<a id="FutureStateGoLink" href="$Page.Link">Go</a>
 </div>
