14712dbb1ef13137abb548a672f9a4f37d21c5bc
Tom Rix <tom@rixth.org>
2010-02-16 20:06:08 +0000

    BUGFIX: Reverted erroneous commit in r90302 (from r91759) (from r95220)

--- code/ThreeStep/batchactions/BatchPublishPages.php
+++ code/ThreeStep/batchactions/BatchPublishPages.php
@@ -12,32 +12,6 @@ class BatchPublishPages extends CMSBatchAction {
 		$pageIDs = $pages->column('ID');
 
 		foreach($pageIDs as $pageID) FormResponse::add("$('Form_EditForm').reloadIfSetTo($pageID);");
-
-		$failures = 0;
-		foreach($pages as $page) {
-			// Is the action a deletion or a publication
-			if($wr = $page->openWorkflowRequest('WorkflowRequest')) {
-				
-			}
-
-			// Perform the action
-			if (call_user_func_array(array($page, $helperMethod), $arguments) === false) $failures++;
-			
-			// Now make sure the tree title is appropriately updated
-			$publishedRecord = DataObject::get_by_id('SiteTree', $page->ID);
-			if ($publishedRecord) {
-				$JS_title = Convert::raw2js($publishedRecord->TreeTitle());
-				FormResponse::add("\$('sitetree').setNodeTitle($page->ID, '$JS_title');");
-			}
-			$page->destroy();
-			unset($page);
-		}
-
-		$message = sprintf($successMessage, $pages->Count()-$failures, $failures);
-		FormResponse::add('statusMessage("'.$message.'","good");');
-
-		return FormResponse::respond();
-
 		
 		$this->batchaction($pages, 'batchPublish',
 			_t('BatchPublishPages.PUBLISHED_PAGES', 'Published %d pages, %d failures')
