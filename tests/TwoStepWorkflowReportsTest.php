42627620da366597f10807296e1c22a847714be4
Tom Rix <tom@rixth.org>
2010-02-26 01:27:26 +0000

    MINOR two step workflow reports tests

---
+++ tests/TwoStepWorkflowReportsTest.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @package cmsworkflow
+ * @subpackage tests
+ */
+class TwoStepWorkflowReportsTest extends FunctionalTest {
+	
+	static $fixture_file = 'cmsworkflow/tests/TwoStepWorkflowReportsTest.yml';
+
+	protected $requiredExtensions = array(
+		'SiteTree' => array('SiteTreeCMSTwoStepWorkflow'),
+		'WorkflowRequest' => array('WorkflowTwoStepRequest'),
+	);
+
+	protected $illegalExtensions = array(
+		'SiteTree' => array('SiteTreeCMSThreeStepWorkflow'),
+		'WorkflowRequest' => array('WorkflowThreeStepRequest'),
+		'LeftAndMain' => array('LeftAndMainCMSThreeStepWorkflow'),
+		'SiteConfig' => array('SiteConfigThreeStepWorkflow'),
+	);
+	
+	function testDeletionRequestsINeedToApproveReport() {
+		$report = new MyTwoStepDeletionRequests();
+		$this->assertTrue(is_string($report->title()));
+		$this->assertTrue(is_string($report->group()));
+		$this->assertTrue(is_numeric($report->sort()));
+		$this->assertTrue(is_array($report->columns()));
+		$this->assertTrue($report->canView());
+		
+		$publisher = $this->objFromFixture('Member', 'publisher');
+		$admin = $this->objFromFixture('Member', 'admin');
+		
+		$this->logInAs($publisher);
+		$results = $report->sourceRecords(array());
+		$this->assertContains($this->objFromFixture('SiteTree', 'page2')->ID, $results->column('ID'));
+		$this->assertEquals(1, $results->Count());
+		
+		$this->logInAs($admin);
+		$this->assertEquals(2, $report->sourceRecords(array())->Count());
+	}
+	
+	function testPublicationRequestsINeedToApproveReport() {
+		$report = new MyTwoStepPublicationRequests();
+		$this->assertTrue(is_string($report->title()));
+		$this->assertTrue(is_string($report->group()));
+		$this->assertTrue(is_numeric($report->sort()));
+		$this->assertTrue(is_array($report->columns()));
+		$this->assertTrue($report->canView());
+		
+		$publisher = $this->objFromFixture('Member', 'publisher');
+		$admin = $this->objFromFixture('Member', 'admin');
+		
+		$this->logInAs($publisher);
+		$results = $report->sourceRecords(array());
+		$this->assertContains($this->objFromFixture('SiteTree', 'page1')->ID, $results->column('ID'));
+		
+		$this->logInAs($admin);
+		$this->assertEquals(2, $report->sourceRecords(array())->Count());
+	}
+	
+	function testMyRequestsPendingReview() {
+		$report = new MyTwoStepWorkflowRequests();
+		$this->assertTrue(is_string($report->title()));
+		$this->assertTrue(is_string($report->group()));
+		$this->assertTrue(is_numeric($report->sort()));
+		$this->assertTrue(is_array($report->columns()));
+		$this->assertTrue($report->canView());
+		
+		$author = $this->objFromFixture('Member', 'author');
+		$this->logInAs($author);
+		
+		$this->assertContains($this->objFromFixture('SiteTree', 'page1')->ID, 
+			$report->sourceRecords(array())->column('ID'));
+		$this->assertContains($this->objFromFixture('SiteTree', 'page2')->ID, 
+			$report->sourceRecords(array())->column('ID'));
+	}
+
+}
+?>
