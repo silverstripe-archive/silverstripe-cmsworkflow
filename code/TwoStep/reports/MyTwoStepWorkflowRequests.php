d77696219568af11ad9bfe0da8f5f38ee945a23e
Mark Stephens <mark@silverstripe.com>
2010-06-17 02:15:29 +0000

    BUGFIX: 'my requests' report doesn't pick up deletions, because they have been deleted

--- code/TwoStep/reports/MyTwoStepWorkflowRequests.php
+++ code/TwoStep/reports/MyTwoStepWorkflowRequests.php
@@ -14,19 +14,48 @@ class MyTwoStepWorkflowRequests extends SS_Report {
 	function sort() {
 		return 100;
 	}
+
+	/**
+	 * This returns the workflow requests outstanding for this user.
+	 * It does one query against draft for change requests, and another
+	 * request against live for the deletion requests (which are not in draft
+	 * any more), and merges the result sets together.
+	 */
 	function sourceRecords($params) {
-		return WorkflowTwoStepRequest::get_by_author(
-			'WorkflowRequest',
+		$currentStage = Versioned::current_stage();
+
+		$changes = WorkflowTwoStepRequest::get_by_author(
+			'WorkflowPublicationRequest',
 			Member::currentUser(),
 			array('AwaitingApproval')
 		);
+		if ($changes) foreach ($changes as $change) $change->RequestType = "Publish";
+
+		Versioned::reading_stage(Versioned::get_live_stage());
+
+		$deletions = WorkflowTwoStepRequest::get_by_author(
+			'WorkflowDeletionRequest',
+			Member::currentUser(),
+			array('AwaitingApproval')
+		);
+		if ($deletions) foreach ($deletions as $deletion) $deletion->RequestType = "Deletion";
+
+		if ($changes && $deletions) $changes->merge($deletions);
+		else if ($deletions) $changes = $deletions;
+
+		return $changes;
 	}
+
 	function columns() {
 		return array(
+			"RequestType" => array(
+				"title" => "Type",
+				"link" => false
+			),
 			"Title" => array(
 				"title" => "Title",
 				"link" => true,
-			),
+			)
 		);
 	}
 	function canView() {
