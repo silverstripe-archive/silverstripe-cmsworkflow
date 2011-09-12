276d8cd623c38ba013d44b789f72841548c73b9b
Ingo Schommer <ingo@silverstripe.com>
2011-09-12 11:34:16 +0200

    MINOR Using SS_Log for logging task status in ScheduledPublishing, to ensure it includes a timestamp and uses core consistently. Logging page URL for more specific debug information (AIR-61)

--- code/ScheduledPublishing.php
+++ code/ScheduledPublishing.php
@@ -17,6 +17,7 @@ class ScheduledPublishing extends BuildTask {
 	function getDescription() {
 		return 'Publish changes that are scheduled';
 	}
+
 	function getTitle() {
 		return 'CMS Workflow: embargo/expiry';
 	}
@@ -27,14 +28,18 @@ class ScheduledPublishing extends BuildTask {
 	 * @param SS_HTTPRequest $httpRequest 
 	 */
 	function run($httpRequest) {
+		require_once 'Zend/Log/Writer/Stream.php';
+		SS_Log::add_writer(new Zend_Log_Writer_Stream('php://output'), SS_Log::NOTICE);
+		
 		Cookie::$report_errors = false;
 		if (class_exists('Subsite')) Subsite::$disable_subsite_filter = true;
 
 		if (class_exists('Subsite')) Subsite::$disable_subsite_filter = true;
 		
-		// Look for changes that need to be published
+		$this->log('Looking for changes that need to be published');
 		$bt = defined('DB::USE_ANSI_SQL') ? "\"" : "`";
 		$wfRequests = DataObject::get('WorkflowRequest', "{$bt}Status{$bt} = 'Scheduled' AND {$bt}EmbargoDate{$bt} <= '".SS_Datetime::now()->getValue()."'");
+		$this->log(sprintf('Found %d pages', $wfRequests ? count($wfRequests) : 0));
 		$admin = Security::findAnAdministrator();
 		$admin->logIn();
 		
@@ -44,48 +49,60 @@ class ScheduledPublishing extends BuildTask {
 				// Use a try block to prevent one bad request
 				// taking down the whole queue
 				try {
-					if (!$this->suppressOutput) echo "\n<br />Attempting to publish ".$request->Page()->Title.": ";
+					$page = $request->Page();
+					$this->log(sprintf("Attempting to publish '%s' (URL: %s)", $page->Title, $page->AbsoluteLink()));
 					// We remove the embargo date and republish to trigger this.
 					$request->EmbargoDate = null;
 					$result = $request->publish('Page was embargoed. Automatically published.', WorkflowSystemMember::get(), false);
-					if (!$this->suppressOutput) echo "ok.";
+					$this->log(sprintf("Published '%s' (URL: %s)", $page->Title, $page->AbsoluteLink()));
 				} catch (Exception $e) {
 					// Log it?
-					if (!$this->suppressOutput) echo "fail.";
+					$this->log(sprintf("Failed to publish '%s (URL: %s)", $page->Title, $page->AbsoluteLink()));
 					user_error("Error publishing change to Page ID ".$request->PageID." - ".$request->Page()->Title." Error: ".$e->getMessage(), E_USER_WARNING);
 					continue;
 				}
 			}
 		}
 		
-		// Look for live pages that need to be expired
+		$this->log('Looking for live pages that need to be expired');
 		$pagesToExpire = Versioned::get_by_stage('SiteTree', 'Live', "\"ExpiryDate\" <= '".SS_Datetime::now()->getValue()."'");
+		$this->log(sprintf('Found %d pages', $pagesToExpire ? count($pagesToExpire) : 0));
 		if (count($pagesToExpire)) {
 			foreach($pagesToExpire as $page) {
 				// Use a try block to prevent one bad request
 				// taking down the whole queue
 				try {
-					if (!$this->suppressOutput) echo "\n<br />Attempting to unpublish ".$page->Title.": ";
+					$this->log(sprintf("Attempting to unpublish '%s' (URL: %s)", $page->Title, $page->AbsoluteLink()));
 					
 					// Close any existing workflows
 					if ($wf = $page->openWorkflowRequest()) {
-						if (!$this->suppressOutput) echo "closing ".$wf->Status." workflow request: ";
+						$this->log(sprintf("Closing '%s' workflow request for '%s'", $wf->Status, $page->Title));
 						$wf->deny('Page automatically expired. Removing from Live site.', $admin);
-						if (!$this->suppressOutput) echo "ok, unpublishing: ";
 					}
 
 					$page->ExpiryDate = null;
 					$page->write();
 					$page->doUnpublish();
 					
-					if (!$this->suppressOutput) echo "ok.";
+					$this->log(sprintf("Unpublished '%s' (URL: %s)", $page->Title, $page->AbsoluteLink()));
 				} catch (Exception $e) {
-					// Log it?
-					if (!$this->suppressOutput) echo "fail.";
+					
+					$this->log(sprintf("Failed to unpublish '%s' (URL: %s)", $page->Title, $page->AbsoluteLink()));
 					user_error("Error unpublishing Page ID ".$page->ID." - ".$page->Title." Error: ".$e->getMessage(), E_USER_WARNING);
 					continue;
 				}
 			}
+		}
+	}
+	
+	/**
+	 * @param String
+	 */
+	protected function log($message) {
+		if(!$this->suppressOutput) {
+			// TODO Only works because we're using a Zend writer rather that
+			// SS_Log* writers, which assume the argument to be an array or exception, breaking the base API.
+			SS_Log::log($message, SS_Log::NOTICE);
 		}
 	}
 }
